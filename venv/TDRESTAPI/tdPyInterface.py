b58f8856d009a2afd5d411ffbeb1c1691e502b23
Satya Mohapatra <smohapatra@shutterfly.com>
2017-08-01 21:59:24 -0700

    Added updates to HelloWorld.py
    -- Tested on dev
    
    Updates:
    - works for all cols and vals

--- venv/TDRESTAPI/tdPyInterface.py
+++ venv/TDRESTAPI/tdPyInterface.py
@@ -1,5 +1,6 @@
-from flask import Flask, url_for, request, render_template, Response, jsonify
+from flask import Flask, url_for, request, render_template, Response, jsonify, json
 import teradata
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
@@ -10,18 +11,40 @@ def index():
     return "Welcome to tdPyInterface"
 
 
-@app.route('/<qry>', methods=['GET'])
-def show_qry_result(qry):
+@app.route('/tddev/tplib/<query>', methods=['GET'])
+def show_qry_result(query):
     data = []
-    print ("SQL ISSUED : %s" % qry)
+    print ("SQL ISSUED : %s" % query)
     udaExec = teradata.UdaExec(appName="tdPyInterface", version="1.0",
                                logConsole=False, appConfigFile="tdPyInterface.ini")
     session = udaExec.connect(method="odbc", dsn="TDDEV",
                               username="crmp_su", password="crmp_pass")
-    for row in session.execute(qry):
+
+    # session = udaExec.connect(method="rest", dsn="TDDEV", username="crmp_su",
+    #                             password="crmp_pass")
+
+    for row in session.execute(query):
         data.append(str(row))
 
+
+
     return jsonify(results=data)
+
+
+@app.route('/tddev/sa/<query>')
+def show_sa_results(query):
+    # return 'Query issued: %s' % query
+
+    user = 'crmp_user'
+    pasw = 'crmp_pass'
+    # host = '172.18.48.172'
+    dsn = 'tddev'
+
+    td_engine = create_engine('teradata://' + user + ':' + pasw + '@' + dsn + ':22/crmp')
+    # return str(td_engine)
+    result = td_engine.execute(query)
+
+    return str(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
